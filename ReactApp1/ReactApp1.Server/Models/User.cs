4686a8a943eb5a52d6f997d3ce29dc9509583c8e
szmirtill <mirtillszentes18@gmail.com>
2025-04-10 08:30:16 +0200

    doc

--- ReactApp1/ReactApp1.Server/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyApp.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-    }
-}
